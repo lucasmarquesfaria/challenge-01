7c2afd937c874d299623469f4fa0d51cd9fe393b
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-06-14 14:04:26 -0300

    chore: config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,38 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/no-array-index-key': 'warn',
+    'react/jsx-key': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-console': 'warn',
+    'no-var': 'error',
+    curly: 'error',
   },
 };
